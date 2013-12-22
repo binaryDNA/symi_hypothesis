53ba849fff8cb07a421ab03efe6b73fcf2a64432
binaryDNA <dm@mitsinikos.net>
2013-12-22 14:51:53 +0000

    Update symi_hypothesis_plot.py

--- symi_hypothesis_plot.py
+++ symi_hypothesis_plot.py
@@ -1,3 +1,14 @@
+# symi_hypothesis
+# ===============
+#
+# This python script shows the SYMI Hypothesis in action. 
+# Copyright: Dimitrios Mitsinikos.
+#
+# For more information, please go to 
+# http://dna.mitsinikos.net
+# http://dna.mitsinikos.net/symi-hypothesis
+
+
 from pylab import *
 from math import *
 
